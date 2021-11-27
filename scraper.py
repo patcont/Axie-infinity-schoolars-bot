9279411bea6ec2f2284e471d4a7c518cecb7a010
patoxo <89327364+patoxo@users.noreply.github.com>
2021-11-27 11:31:10 -0400

    comments added

--- scraper.py
+++ scraper.py
@@ -5,11 +5,18 @@ import os
 from asyncio import events
 from discord.ext import commands
 
+#You can use many urls as you want, they are from the site axie.management,
+#after you have entered your schoolars ronin wallets in their site for tracking.
 url = "https://api.axie.management/v1/overview/ronin_wallet_0"
 
 url_2 = "https://api.axie.management/v1/overview/ronin_wallet_1"
 
 url_3 = "https://api.axie.management/v1/overview/ronin_wallet_2"
+
+#url_n (n means an infinite number of wallets you cand add depending on how many schoolars you have)
+
+#The code below was taken from the application "insomnia" which call the APIs of each wallet,
+#as the site doesn't allow to scrap directly we need this software to extrac the information of the APIs of each wallet.
 
 payload = ""
 headers = {
@@ -27,12 +34,14 @@ headers = {
    "if-none-match": "W/ea-N84iKYoma0y7hc44E6AxTSQRr6Q"
 }
 
-
+#every response is linked to their respective url, url is correlated to response, url_2 to response_2 and so on"
 response = requests.request("GET", url, data=payload, headers=headers).text
 response_2 = requests.request("GET", url_2, data=payload, headers=headers).text
 response_3 = requests.request("GET", url_3, data=payload, headers=headers).text
+#response_n (n means you cand add as many responses depending on how many schollars you have)
 
 
+#This is the main function than when called by the bot is gives the information we need
 def scraping(r):
 
  y = json.loads(r)
@@ -67,7 +76,7 @@ def scraping(r):
 
  return promedio
 
-
+#commands for the bot in the discord server
 client = commands.Bot(command_prefix = ".")
 
 
@@ -94,5 +103,11 @@ async def player_2(ctx):
 async def player_3(ctx):
    await ctx.send(scraping(response_3)) 
    os.execv(sys.executable, ['python'] + sys.argv)
+
+#you cand add as many @client.command depending on how many schoolars you have
+# clent.command()
+#async def player_n(ctx):
+# await ctx.send(scraping(response_n)) 
+ # os.execv(sys.executable, ['python'] + sys.argv) 
 
 client.run('TOKEN')   
